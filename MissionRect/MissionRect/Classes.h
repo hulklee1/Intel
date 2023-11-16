e1145117fdbb9189ba12d2a21cc42f7b2cb444de
Wardell Lee <HulkLee@gmail.com>
2023-11-16 16:52:38 +0900

    1

--- MissionRect/MissionRect/Classes.h
+++ MissionRect/MissionRect/Classes.h
@@ -1,14 +1,25 @@
 #pragma once
 #include <iostream>
-#define		ABS(x)	(((x)<0)?(-(x)):(x))
+#define		ABS(x)		(((x)<0)?(-(x)):(x))
+#define		MIN(x,y)	(((x)<(y))?(x):(y))
+#define		MAX(x,y)	(((x)>(y))?(x):(y))
 class Point
 {
 private:
 public:
 	int x;
 	int y;
+	Point* p1 = NULL;
 
-	Point(int x1 = 0, int y1 = 0) : x(x1), y(y1) {}
+	Point(int x1 = 0, int y1 = 0) : x(x1), y(y1) 
+	{
+		std::cout << "Point »ý¼º(" << x << "," << y << ")\n";
+	}
+	~Point()
+	{
+		std::cout << "Point ¼Ò¸ê(" << x << "," << y << ")\n";
+		if (p1 != NULL) delete p1;
+	}
 
 	double Dist(Point p);		// Distance
 	void Show(const char* s = NULL);  // View Current Point, s:Point name
@@ -21,12 +32,33 @@ class Rect
 {
 public:
 	Point p1, p2;
-
-	Rect(Point pp1, Point pp2) : p1(pp1), p2(pp2) {}
-	Rect(int x1 = 0, int y1 = 0, int x2 = 0, int y2 = 0)
+	Point LL, LR, UL, UR;
+	int x1, x2, y1, y2;
+private:
+	void Init()
 	{
-		p1 = Point(x1, y1);
-		p2 = Point(x2, y2);
+		LL = Point(MIN(p1.x, p2.x), MIN(p1.y, p2.y));
+		LR = Point(MAX(p1.x, p2.x), MIN(p1.y, p2.y));
+		UL = Point(MIN(p1.x, p2.x), MAX(p1.y, p2.y));
+		UR = Point(MAX(p1.x, p2.x), MAX(p1.y, p2.y));
 	}
-	void Show(const char* s);  // RECT(P1(x1,y1), P2(x2,y2))
+public:
+	Rect(Point pp1, Point pp2) : p1(pp1), p2(pp2), 
+		x1(pp1.x), y1(pp1.y), x2(pp2.x), y2(pp2.y) 
+	{
+		Init();
+	}
+	Rect(int xx1 = 0, int yy1 = 0, int xx2 = 0, int yy2 = 0)
+	{
+		x1 = xx1; y1 = yy1;
+		x2 = xx2; y2 = yy2;
+		p1 = Point(xx1, yy1);
+		p2 = Point(xx2, yy2);
+		Init();
+	}
+	double Area();
+	Rect& Show(const char* s);  // RECT(P1(x1,y1), P2(x2,y2))
+	Rect& operator+(Rect r);
+	Rect& operator-(Rect r);
+	double operator*(Rect r);
 };
