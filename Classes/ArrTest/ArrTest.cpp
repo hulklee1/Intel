96bddebfe1d5dea2cf41adc3301e865c357a47ab
Wardell Lee <HulkLee@gmail.com>
2023-11-27 17:52:57 +0900

    1

--- Classes/ArrTest/ArrTest.cpp
+++ Classes/ArrTest/ArrTest.cpp
@@ -1,14 +1,15 @@
 ﻿#include <iostream>
 
+template <typename T>
 class ArrTest
 {
 private:
-    int *arr;
-    int Len;
+    T *arr;
+    int Len;  // 배열 요소의 수
 public:
     ArrTest(int size) : Len(size)
     {
-        arr = new int[size];
+        arr = new T[size];
     }
     ~ArrTest()
     {
@@ -18,7 +19,7 @@ public:
     {
         return Len;
     }
-    int& operator[](int idx)   // calling sequence : arr[n]
+    T& operator[](int idx)   // calling sequence : arr[n]
     {
         if (idx<0 || idx > Len-1)
         {
@@ -26,28 +27,37 @@ public:
         }
         return arr[idx];
     }
-    ArrTest& append(int size);
-    ArrTest& append(ArrTest& brr);
+    void show()
+    {
+        int i;
+        printf("{");
+        for (i = 0; i < Len - 1; i++)
+            std::cout << arr[i] << ",";    std::cout << arr[i];
+        printf(" }\n");
+    }
+    ArrTest& append(int size)
+    {
+        T *arr1 = new T[Len + size];  // 확장된 메모리
+        memcpy(arr1, arr, Len * sizeof(T));
+        delete arr; 
+        arr = arr1;
+        Len += size;
+        return *this;
+    }
+    ArrTest& append(ArrTest& brr)
+    {
+        T *arr1 = new T[Len + brr.length()];  // 확장된 메모리
+        memcpy(arr1, arr, Len * sizeof(T));
+        memcpy(arr1 + Len, brr.arr, brr.length() * sizeof(T));
+        delete arr; 
+        arr = arr1;
+        Len += brr.length();
+        return *this;
+    }
 };
-ArrTest& ArrTest::append(int size)  // size :  total ?  
-{
-    int *arr1 = new int[Len + size];  // 확장된 메모리
-    memcpy(arr1, arr, Len * sizeof(int));
-    delete arr; 
-    arr = arr1;
-    Len += size;
-    return *this;
-}
-ArrTest& ArrTest::append(ArrTest& brr)  // size :  total ?  
-{
-    int *arr1 = new int[Len + brr.length()];  // 확장된 메모리
-    memcpy(arr1, arr, Len * sizeof(int));
-    memcpy(arr1 + Len, brr.arr, brr.length() * sizeof(int));
-    delete arr; 
-    arr = arr1;
-    Len += brr.length();
-    return *this;
-}
+//void ArrTest<T>::show()   // { 1 2 3 4 5} ==> { 1,2,3,4,5 }
+//ArrTest& ArrTest::append(int size)  // size :  total ?  
+//ArrTest& ArrTest::append(ArrTest& brr)  // size :  total ?  
 
 class Func
 {
@@ -60,22 +70,26 @@ public:
     {
         return a * b;
     }
-    ArrTest& operator()(ArrTest& b1, ArrTest& b2)  // size :  total ?  
+    ArrTest<int>& operator()(ArrTest<int>& b1, ArrTest<int>& b2)  // size :  total ?  
     {
-
+        return b1.append(b2);
+    }
+    ArrTest<double>& operator()(ArrTest<double>& b1, ArrTest<double>& b2)  // size :  total ?  
+    {
+        return b1.append(b2);
     }
 };
 //int func(int arg) {}
 int main()
 {
-    //int a1[] = { 15,12,13,11,14 };
-    //ArrTest arr(7);    
-    //for (int i = 0; i < 5; i++)             arr[i] = a1[i];
+    double a1[] = { 15.1,12.1,13.1,11.1,14.1 };
+    ArrTest<double> arr(5);
+    for (int i = 0; i < 5; i++)             arr[i] = a1[i];
     //for (int i = 0; i < arr.length() ; i++) printf("ArrTest[%d] = %d\n", i, arr[i]);
     //
-    //int a2[] = { 21,22,23,24,25 };
-    //ArrTest brr(7);
-    //for (int i = 0; i < 5; i++)             brr[i] = a2[i];
+    double a2[] = { 21.1,22.1,23.1,24.1,25.1 };
+    ArrTest<double> brr(5);
+    for (int i = 0; i < 5; i++)             brr[i] = a2[i];
     //for (int i = 0; i < brr.length() ; i++) printf("ArrTest[%d] = %d\n", i, brr[i]);
     //
     //int n = arr.length();
@@ -85,4 +99,5 @@ int main()
     Func mul;
     std::cout << mul(2, 3)<<"\n";
     std::cout << mul(2.5, 3.7)<<"\n";
+    mul(arr, brr).show();
 }
